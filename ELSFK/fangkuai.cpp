9e8f8072e8ef90693aea76f518b1ac5626485508
TheBoyMasterGuy <cross1025@163.com>
2020-02-05 13:14:03 +0800

    ELSFK1

--- ELSFK/fangkuai.cpp
+++ ELSFK/fangkuai.cpp
@@ -16,3 +16,39 @@ int getrand(int min, int max)
 {
     return (min+rand()%(max-min+1));
 }
+
+class Piece
+{
+    public:
+        int score;
+        int shape;
+        int next_shape;
+
+        int head_x;
+        int head_y;
+
+        int size_h;
+        int size_w;
+
+        int next_size_h;
+        int next_size_w;
+
+        int box_shape[4][4];
+        int next_box_shape[4][4];
+
+        int box_map[30][45];
+
+        bool game_over;
+
+    public:
+        void initial();
+        void set_shape(int &cshape, int box_shape[][4], int &size_w, int &size_h);
+
+        void score_next();
+        void judge();
+        void move();
+        void rotate();
+        bool isaggin();
+        bool exsqr(int row);
+
+};
